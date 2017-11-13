99527eed85f26bb7cdf58c7f7a947669e4bec89a
gtrianta1965 <george.triantafylidis@gmail.com>
2017-11-13 13:13:57 +0200

    Change Cache Control

--- ElvalRestApi/src/main/java/com/elval/alert/NotificationService.java
+++ ElvalRestApi/src/main/java/com/elval/alert/NotificationService.java
@@ -1,15 +1,18 @@
 package com.elval.alert;
 
 
-
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -27,13 +30,17 @@ public class NotificationService {
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public Response getActiveNotifications() {
         Response r;
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(1);
         QueryNotifications qm = new QueryNotifications();
         NotificationMessage nt = new NotificationMessage();
         nt = qm.queryActiveNotifications();
         
         LOGGER.info("getActiveNotifications called");
         if (nt.getMessage() != null) {
-            r = Response.status(200).entity(nt).build();
+            long current = new Date().getTime();
+            Date expires = new Date(current + 10);
+            r = Response.status(200).cacheControl(cc).entity(nt).build();
         } else {
             if (nt.getError() != null) {
                 r = Response.status(400).entity(nt).build();
